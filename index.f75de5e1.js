ea73a08d797884c83cbb668748325297df1eba3e
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-08-13 19:41:24 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,38 @@
+"use strict";
+const listEmployes = [
+    ...document.getElementsByTagName("li")
+];
+function sortList() {
+    const salaryArr = listEmployes.map((el)=>{
+        const salary = el.dataset.salary;
+        const numericSalary = Number(salary.replace(/[\$,]/g, ""));
+        return {
+            element: el,
+            salary: numericSalary
+        };
+    });
+    salaryArr.sort((fEl, sEl)=>sEl.salary - fEl.salary);
+    const parentLi = listEmployes[0].parentNode;
+    salaryArr.forEach(({ element })=>{
+        parentLi.appendChild(element);
+    });
+}
+function getEmployees() {
+    const objectOfEmployees = listEmployes.map((el)=>{
+        const name = el.textContent.trim();
+        const position = el.dataset.position;
+        const salary = el.dataset.salary;
+        const age = el.dataset.age;
+        return {
+            name,
+            position,
+            salary,
+            age
+        };
+    });
+    return objectOfEmployees;
+}
+sortList();
+console.log(getEmployees());
+
+//# sourceMappingURL=index.f75de5e1.js.map
